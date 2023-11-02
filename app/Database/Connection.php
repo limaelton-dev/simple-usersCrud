b149dbb9151882f39f8e4880cea61be120e03667
Elton Lima <eltonlima.contato@gmail.com>
2023-11-02 13:58:12 -0300

    criando a conexão com banco de dados

--- app/Database/Connection.php
+++ app/Database/Connection.php
@@ -1,1 +1,36 @@
-<?
+<?php
+
+namespace app\database;
+
+use PDO;
+use PDOException;
+
+class Connection 
+{
+    private static $pdoInstance = null;
+
+    public static function getConnection()
+    {
+        if(empty(self::$pdoInstance)) {
+            self::$pdoInstance = new PDO(
+                "mysql:
+                    host=localhost;
+                    dbname=userscrud",
+                "root",
+                "1234",
+                [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]
+            );
+        }
+
+        return self::$pdoInstance;
+    }
+}
+
+// try {
+
+//     $pdo = Connection::getConnection();
+
+//     echo "conectou!";
+// } catch(PDOException $e) {
+//     die("não conectou: ". $e->getMEssage());
+// }
